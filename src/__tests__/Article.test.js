44b1f08561cf87dd02c4a8829ae5da9fefe76601
Adrianna <adriannahdbell@gmail.com>
2024-05-13 13:29:22 -0700

    edited article once again

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,8 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+
+  // add this line
   screen.debug();
 
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
