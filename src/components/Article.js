5b72b675f5ecc8160c318c901570e124c6ef2b3c
Adrianna <adriannahdbell@gmail.com>
2024-05-13 13:39:52 -0700

    removed divs

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      <h1>Please pass this test!</h1>;
-    </div>
-  );
+  return <h1>Please pass this test!</h1>;
 }
 
 export default Article;
